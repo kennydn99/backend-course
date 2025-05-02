74ab3b836d4ea7a16211724f131ebba00d435a20
kennydn99 <kennydn99@gmail.com>
2025-05-01 23:09:07 -0700

    Setup frontend and serve at home endpoint

---
+++ chapter_3/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Get file path from URL of current module
+const __filename = fileURLToPath(import.meta.url);
+
+// Get dir name from file path
+const __dirname = dirname(__filename);
+
+// Serves HTML file from the /public dir
+// tells express to serve all filels from public folder as static assets/files.
+// Any requests for css files will be resoved to public dir
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Serve up HTML file from /publilc dir
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
+});
