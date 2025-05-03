eaef666fff32d4476a227a851ae4a654cf6f4f2d
kennydn99 <kennydn99@gmail.com>
2025-05-02 20:14:26 -0700

    setup database with sqlite

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -7,10 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 // Get file path from URL of current module
 const __filename = fileURLToPath(import.meta.url);
-
 // Get dir name from file path
 const __dirname = dirname(__filename);
 
+// Middleware
+app.use(express.json());
 // Serves HTML file from the /public dir
 // tells express to serve all filels from public folder as static assets/files.
 // Any requests for css files will be resoved to public dir
