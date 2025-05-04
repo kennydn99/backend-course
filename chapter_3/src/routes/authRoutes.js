df62f92e6378a90e9395f77fcf5f34cf10238189
kennydn99 <kennydn99@gmail.com>
2025-05-04 11:18:33 -0700

    setup login route

--- chapter_3/src/routes/authRoutes.js
+++ chapter_3/src/routes/authRoutes.js
@@ -38,6 +38,33 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {});
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
+    const user = getUser.get(username);
+
+    // if user not found, return out of function
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    // if password does not match, return out of function
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "invalid password" });
+    }
+    console.log(user);
+    // then we have successful authentication
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(503);
+  }
+});
 
 export default router;
