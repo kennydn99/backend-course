12c721860eb0bac984a3ef79b952e6fd4b3edab1
kennydn99 <73307872+kennydn99@users.noreply.github.com>
2025-05-05 12:21:40 -0700

    Set up prisma orm and update routes to user prismaclient for chapter 4

---
+++ chapter_4/src/routes/authRoutes.js
@@ -0,0 +1,78 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import db from "../db.js";
+import prisma from "../prismaClient.js";
+
+const router = express.Router();
+
+// Register new user endpoint /auth/register
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  // encrypt pwd
+  const hashPassword = bcrypt.hashSync(password, 8);
+
+  // save new user aand hashedpasswrod to db
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashPassword,
+      },
+    });
+
+    // create default todo for user
+    const defaultTodo = "Hello, add your first todo!";
+
+    await prisma.todo.create({
+      data: {
+        task: defaultTodo,
+        userId: user.id,
+      },
+    });
+
+    // create a token
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(503);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
+
+    // if user not found, return out of function
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+    // if password does not match, return out of function
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "invalid password" });
+    }
+    console.log(user);
+    // then we have successful authentication
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(503);
+  }
+});
+
+export default router;
---
+++ chapter_4/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
+import authMiddleware from "./middleware/authMiddleware.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Get file path from URL of current module
+const __filename = fileURLToPath(import.meta.url);
+// Get dir name from file path
+const __dirname = dirname(__filename);
+
+// Middleware
+app.use(express.json());
+// Serves HTML file from the /public dir
+// tells express to serve all filels from public folder as static assets/files.
+// Any requests for css files will be resoved to public dir
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Serve up HTML file from /public dir
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/todos", authMiddleware, todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port: ${PORT}`);
+});
